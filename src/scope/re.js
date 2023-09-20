34dbc8f0e54b47be94896842b9c6099824670450
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-20 13:53:15 -0600

    redeclare js

---
+++ src/scope/re.js
@@ -0,0 +1,30 @@
+var firstName; // Undefined
+firstName = "German";
+console.log(firstName);
+
+var lastName = "Adrian"; // declarar / asignar
+lastName = "Lizbeth"; // reasignar
+console.log(lastName);
+
+var secondName = "Ernesto"; // declarando / asignando
+var secondName = "Terrones"; // reasignando
+console.log(secondName);
+
+// let
+let fruit = "Apple"; // declara y asignar
+fruit = "Kiwi"; // reasignar
+console.log(fruit);
+fruit = "Pene";
+
+// const
+const animal = "dog";
+// animal = "cat";
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("👵");
+console.log(vehicles);
+
+vehicles.pop();
+vehicles.push("👿");
+console.log(vehicles);
