05f092e8377aadb5ca5ba950fcd18e6df3c0dc79
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-20 12:10:58 -0600

    global js

---
+++ src/scope/global.js
@@ -0,0 +1,15 @@
+// variables
+
+var a; // declarando
+var b = "b"; // declaramos / asignamos
+b = "bb"; // reasignación
+var a = "aa"; // redeclaración
+
+// Globar Scope
+var fruit = "Apple"; // global
+
+function bestFruit() {
+  console.log(fruit);
+}
+
+bestFruit();
