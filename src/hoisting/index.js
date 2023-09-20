43b312e1a8f7ecfed1ce4e4b93d62d0f46c5e075
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-20 16:16:22 -0600

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+// var nameOfDog; //undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = "Elmo";
+// console.log(nameOfDog);
+
+// var elmo; //undefined
+var elmo = "tumama";
+
+function nameOfDog() {
+  console.log(`El mejor es ${elmo}`);
+}
+nameOfDog();
+
+// var elmo = "tumama"; // si se declara aqui dara undefined
